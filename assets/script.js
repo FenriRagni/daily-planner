753cfdc0e99acedfc444385ac1b9a4dc59270740
FenriRagni <145153161+FenriRagni@users.noreply.github.com>
2023-10-04 11:02:17 -0400

    add eventlistener on btn and save textarea to localStorage

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
+var currentHour = dayjs();
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -8,7 +11,16 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  $(document).on("click", "button", function(event) {
+    var btn = $(event.target);
+    var textArea = btn.parent().children().eq("1");
+    console.log(event.target);
+    console.log($(event.target));
+    console.log(btn.parent().children().eq("1"));
+    console.log("Button Clicked!");
+    localStorage.setItem(textArea.parent().attr("id"), textArea.val());
+  })
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -21,3 +33,9 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+
+//make multiple timeblocks
+//rename timeblocks according
+//event listener on time-block to delegate to button
+  //
